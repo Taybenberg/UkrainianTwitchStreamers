7479e530e7d8cab866c584e75c32e05ba5727f89
Вдовенко Віталій <taybenberg@gmail.com>
2019-10-11 18:09:11 +0300

    Inline mode

--- TwitchUkrBot/TwitchUkrBot.cs
+++ TwitchUkrBot/TwitchUkrBot.cs
@@ -1,6 +1,8 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.InlineQueryResults;
 
 namespace TwitchUkrBot
 {
@@ -17,6 +19,30 @@ namespace TwitchUkrBot
             var Bot = new TelegramBotClient(TelegramApiToken);
 
             Bot.SetWebhookAsync("");
+
+            Bot.OnInlineQuery += async (object updobj, InlineQueryEventArgs iqea) =>
+            {
+                var list = new UkrainianTwitchStreamers.UkrainianTwitch(TwitchApiToken).ToList();
+                
+                var inline = new InlineQueryResultArticle[list.Count];
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    var content = new InputTextMessageContent($"<b>{list[i].title}</b>\n<i>{list[i].user_name}</i> <b>|</b> <i>{list[i].name}</i>\n{list[i].url}");
+                    content.ParseMode = ParseMode.Html;
+
+                    inline[i] = new InlineQueryResultArticle(
+                      i.ToString(),
+                      list[i].user_name,
+                      content);
+
+                    inline[i].Description = $"{list[i].title} | {list[i].name}";
+
+                    inline[i].ThumbUrl = list[i].url;
+                }
+
+                await Bot.AnswerInlineQueryAsync(iqea.InlineQuery.Id, inline);
+            };
 
             Bot.OnMessage += async (object updobj, MessageEventArgs mea) =>
             {
@@ -38,9 +64,14 @@ namespace TwitchUkrBot
                             break;
 
                         case "streamers":
-                            foreach (var stream in new UkrainianTwitchStreamers.UkrainianTwitch(TwitchApiToken).ToStringArray())
-                                Bot.SendTextMessageAsync(ChatId, stream);
+                            foreach (var stream in new UkrainianTwitchStreamers.UkrainianTwitch(TwitchApiToken).ToList())
+                                Bot.SendTextMessageAsync(ChatId, $"<b>{stream.title}</b>\n<i>{stream.user_name}</i> <b>|</b> <i>{stream.name}</i>\n{stream.url}", ParseMode.Html);
                             break;
+
+                        case "sendstreamer":
+                            await Bot.SendTextMessageAsync(ChatId, "Натисніть кнопку та оберіть чат до якого хочете надіслати стрімера.", replyMarkup: new InlineKeyboardMarkup(new[] { InlineKeyboardButton.WithSwitchInlineQuery("Надіслати") }));
+                            break;
+
                     }
                 }
             };
